9e2be8a705be431abf9a179c94adf803297e7c8f
nataliexists <treefiddy723@gmail.com>
2025-01-03 23:42:31 +1000

    vector arithmetic is now supported!
    have yet to update the readme

--- vector.lua
+++ vector.lua
@@ -28,6 +28,79 @@ mt.__newindex = function(tab, key, value)
   end
 end
 
+local vector_enlarge = function(vector, size)
+  local new = Vec(size)
+
+  local j = 1
+  for i=1, size do
+    new[i] = vector[j]
+
+    j = j + 1
+    if j > #vector then
+      j = 1
+    end
+  end
+
+  return new
+end
+
+local easy_op = function(a, b, f)
+  local new_a, new_b
+
+  if type(a) == "number" then
+    new_a = Vec1(a)
+  else
+    new_a = vector_enlarge(a, #a)
+  end
+
+  if type(b) == "number" then
+    new_b = Vec1(b)
+  else
+    new_b = vector_enlarge(b, #b)
+  end
+
+  if #new_a > #new_b then
+    new_b = vector_enlarge(new_b, #new_a)
+  else
+    new_a = vector_enlarge(new_a, #new_b)
+  end
+
+  local result = Vec(#new_a)
+  for i=1, #new_a do
+    result[i] = f(new_a[i], new_b[i])
+  end
+
+  return result
+end
+
+mt.__add = function(a, b)
+  return easy_op(a, b, function(a,b) return a + b end)
+end
+
+mt.__sub = function(a, b)
+  return easy_op(a, b, function(a,b) return a - b end)
+end
+
+mt.__mul = function(a, b)
+  return easy_op(a, b, function(a,b) return a * b end)
+end
+
+mt.__div = function(a, b)
+  return easy_op(a, b, function(a,b) return a / b end)
+end
+
+mt.__mod = function(a, b)
+  return easy_op(a, b, function(a,b) return a % b end)
+end
+
+mt.__pow = function(a, b)
+  return easy_op(a, b, function(a,b) return a ^ b end)
+end
+
+mt.__unm = function(a, b)
+  return easy_op(a, b, function(a,b) return -a end)
+end
+
 local vector_setup = function(default, size, ...)
   local vec = {}
   for i=1, size do
